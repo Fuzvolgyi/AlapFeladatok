7d7ca01c8f8ddaf101dbe3c35f09fcd43246c2d0
FuzvolgyiZsolt <fuzvolgyi.work@gmail.com>
2019-02-14 16:45:07 +0100

    Észerevételek alapján módosítva, újragondolva

--- Feladat/src/Feladat_3/Feladat_3.java
+++ Feladat/src/Feladat_3/Feladat_3.java
@@ -1,6 +1,5 @@
 package Feladat_3;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Feladat_3 {
@@ -9,29 +8,28 @@ public class Feladat_3 {
 //9. Írjon programot, mely kiírja az első n db páros szám összegét a képernyőre!      
         int vegjel;
         int eredmeny;
-        ArrayList<Integer> szamlista = new ArrayList<>();
+
         System.out.println("Kérek egy egész számot");
         vegjel = szamBeolvas();
         vegjel = Math.abs(vegjel);
-        szamlista = szamListaFeltolt(vegjel, szamlista);
-        eredmeny = nParosSzamOsszege(szamlista);
+        eredmeny = nParosSzamOsszege(vegjel);
         System.out.println("A " + vegjel + " elemből álló számsor "
                 + "páros számainak összege: " + eredmeny);
 
 // 10. Készítsen programot, mely beolvas n db számot a billentyűzetről, majd meghatározza
 //a számok átlagát.       
         vegjel = 5;
-        int beolvasottSzam;
-        szamlista.clear();
-        eredmeny = 0;
-        while (szamlista.size() < 5) {
-            System.out.print("Kérek egy egész számot " + (szamlista.size() + 1)
-                    + "/" + vegjel + " :");
-            beolvasottSzam = szamBeolvas();
-            szamlista.add(beolvasottSzam);
+        int osszeg = 0;
+        int index = 0;
+        double atlag;
+        while (index < 5) {
+            System.out.print("Kérek egy egész számot " + (index + 1) + "/"
+                    + vegjel + " :");
+            osszeg = osszeg + szamBeolvas();
+            index++;
         }
-        eredmeny = szamlistaElemeinekAtlaga(szamlista);
-        System.out.println("A megadott számok átlaga: " + eredmeny);
+        atlag = atlagSzamitas(osszeg, index);
+        System.out.println("A megadott számok átlaga: " + atlag);
 
 //11. Készíts programot, amely (maximum 99 karakter hosszú) mondatokat kér be a
 //felhasználótól, és minden bekérés után kiírja, hogy a megadott mondat palindróma-e!        
@@ -41,12 +39,17 @@ public class Feladat_3 {
 //ékezetes betűket és jeleket. Ha a felhasználó a "kilep" mondatot adta meg, akkor a
 //program lépjen ki!        
         int maxKarakter = 99;
-        String bekertSzoveg;
+        String bekertSzoveg = "";
         String osszehasonlitandoSzoveg;
         String megforditottSzoveg;
-        do {
-            System.out.println("Kérek egy mondatot. (Kilépéshez írd be, hogy \"kilep\".)");
+        while (!bekertSzoveg.equals("kilep")) {
+            System.out.println("Kérek egy mondatot. "
+                    + "(Maximum 99 karakter lehet. "
+                    + "Kilépéshez írd be, hogy \"kilep\".)");
             bekertSzoveg = szovegBeolvaso();
+            if (bekertSzoveg.equals("kilep") || bekertSzoveg.length() > 99) {
+                continue;
+            }
             osszehasonlitandoSzoveg = bekertSzoveg.toLowerCase();
             osszehasonlitandoSzoveg = osszehasonlitandoSzoveg.replaceAll(" ", "");
             megforditottSzoveg = szovegMegfordit(osszehasonlitandoSzoveg);
@@ -55,7 +58,7 @@ public class Feladat_3 {
             } else {
                 System.out.println("A megadott szöveg nem palindrom.");
             }
-        } while (!bekertSzoveg.equals("kilep"));
+        }
 
     }
 
@@ -71,38 +74,31 @@ public class Feladat_3 {
         return szoveg;
     }
 
-    private static ArrayList<Integer> szamListaFeltolt(int vegjel, ArrayList<Integer> szamlista) {
-        for (int i = 0; i < vegjel; i++) {
-            szamlista.add(i);
-        }
-        return szamlista;
-    }
-
-    private static int nParosSzamOsszege(ArrayList<Integer> szamlista) {
-        int eredmeny = 0;
-        for (int i = 0; i < szamlista.size(); i++) {
-            if (szamlista.get(i) % 2 == 0) {
-                eredmeny += szamlista.get(i);
-            }
-        }
-        return eredmeny;
-    }
-
-    private static int szamlistaElemeinekAtlaga(ArrayList<Integer> szamlista) {
-        int eredmeny = 0;
-        for (int i = 0; i < szamlista.size(); i++) {
-            eredmeny += szamlista.get(i);
-        }
-        return eredmeny = eredmeny / szamlista.size();
-    }
-
-    private static String szovegMegfordit(String osszehasonlitandoSzoveg) {
+    private static String szovegMegfordit(String Szoveg) {
         String eredmeny = "";
-        int hossz = osszehasonlitandoSzoveg.length();
-        for (int i = 0; i < hossz; i++) {
-            eredmeny += osszehasonlitandoSzoveg.charAt(hossz - 1 - i);
+        int balFel = Szoveg.length() / 2;
+        int jobbFel = Szoveg.length() - balFel;
+        if (jobbFel > balFel) {
+            eredmeny += Szoveg.charAt(balFel);
+        }
+        for (int i = 0; i < balFel; i++) {
+            eredmeny = Szoveg.charAt(jobbFel + i) + eredmeny
+                    + Szoveg.charAt(balFel - 1 - i);
         }
         return eredmeny;
     }
 
+    private static int nParosSzamOsszege(int vegjel) {
+        int eredmeny = 0;
+        for (int i = 0; i <= vegjel;) {
+            eredmeny += i;
+            i += 2;
+        }
+        return eredmeny;
+    }
+
+    private static double atlagSzamitas(int osszeg, int elemeinekSzama) {
+        double eredmeny = (double) osszeg / elemeinekSzama;
+        return eredmeny;
+    }
 }
